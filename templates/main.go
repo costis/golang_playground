e626c322ccbc9eb39e3960fc424bd31295bc6273
Costis Panagiotopoulos <costisp@gmail.com>
2017-03-23 23:30:28 +0000

    templates: day2

--- templates/main.go
+++ templates/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	//"os"
+	//"strings"
+	//"text/template"
+	"io/ioutil"
+	"fmt"
 	"os"
-	"strings"
-	"text/template"
+	//"io"
 )
 
 type person struct {
@@ -12,39 +16,18 @@ type person struct {
 	Age  int
 }
 
-func main() {
-	//people := []person{
-	//	{"john", 30},
-	//	{"gianni", 10},
-	//}
-
-	helpers := template.FuncMap{
-		"up": func(v string) string { return strings.ToUpper(v) },
-	}
-
-	tmpl := template.Must(template.ParseGlob("./templates/*.html"))
-	tmpl.Funcs(helpers)
-
-	if err := tmpl.ExecuteTemplate(os.Stdout, "index.html", nil); err != nil {
+func directoryListing(path string) {
+	fInfo, err := ioutil.ReadDir(path)
+	if err != nil {
 		log.Fatalln(err)
 	}
 
-	//tmpl.New("footer").Parse("the footer")
-	//tmpl, err := tmpl.New("body").Parse(`
-	//First comes {{ template "header" . }},
-	//
-	//then follows the body with the names:
-	//{{ range . }}
-	//	{{ up "hi" }}
-	//{{ end }}
-	//
-	//and then comes {{ template "footer" }} `)
-	//
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//
-	//tmpl.New("person").Parse(`The person's name is {{ .Name }}'`)
-	//
-	//tmpl.ExecuteTemplate(os.Stdout, "body", people)
+	for _, file := range fInfo {
+		fmt.Println(file.Name())
+	}
+}
+
+func main() {
+	filePath := os.Args[1]
+	directoryListing(filePath)
 }
